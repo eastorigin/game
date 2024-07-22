35125d02fca8e7378f6e76fd7f15d1ce4d51ad8d
Shin Dong Won <eastorigin21@gmail.com>
2024-07-22 17:07:29 +0900

    8일차 강의 내용

---
+++ src/com/ktdsuniversity/edu/game/GameInfo.java
@@ -0,0 +1,63 @@
+package com.ktdsuniversity.edu.game;
+
+public class GameInfo {
+	
+	public final int WORD_LENGTH = 3;
+
+	/**
+	 * 현재(제시)단어
+	 */
+	private String currentWord;
+	
+	/**
+	 * 다음 단어
+	 */
+	private String nextWord;
+	
+	/**
+	 * 마지막 글자
+	 */
+	private String lastLetter;
+	
+	/**
+	 * 게임이 이어나간 횟수
+	 */
+	private int gameCount;
+	
+	/**
+	 * 다음 단어의 길이
+	 */
+	private int nextWordLength;
+	
+	public GameInfo(String currentWord) {
+		this.currentWord = currentWord;
+		this.lastLetter = this.currentWord.substring(this.currentWord.length() - 1);
+	}
+	
+	public void inputNextWord(String nextWord) {
+		this.nextWord = nextWord;
+		this.nextWordLength = this.nextWord.length();
+	}
+	
+	public boolean isEnoughLength() {
+		return this.nextWordLength >= this.WORD_LENGTH;
+	}
+	
+	public boolean isStartLetter() {
+		return this.nextWord.startsWith(this.lastLetter);
+	}
+	
+	public void addCount() {
+		this.gameCount++;
+	}
+	
+	public void changeCurrentWord() {
+		this.currentWord = this.nextWord;
+		this.lastLetter = this.currentWord.substring(this.currentWord.length() -1);
+	}
+	
+	public void gameOver() {
+		System.out.println("게임이 종료되었습니다");
+		System.out.println("이어나간 단어의 수는 " + this.gameCount + "번 입니다");
+	}
+}
